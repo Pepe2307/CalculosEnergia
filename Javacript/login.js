b7bd8dbc9811e0e9137bf196319871e21c482c77
Pablo Paez <pablopaez2307@gmail.com>
2022-11-19 21:01:18 -0300

    save before adding login screen

---
+++ Javacript/login.js
@@ -0,0 +1,14 @@
+/* Esta es una base de datos de prueba,
+en un caso real esto no se estructuraria asi por un tema de seguridad */
+
+baseDeDatos = {
+    1:{
+        user: "pepe",
+        password: "123",
+        privileges: "admin",
+    },
+    2:{},
+    3:{},
+    4:{},
+    5:{},
+}
